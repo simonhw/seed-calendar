456eed25678011c1052e84cb7a5ea343af64a76f
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-01-04 21:42:34 +0000

    Feat: Add switch case to get vegetable attributes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,42 @@ const vegetables = {
 
 // The Calculate button will execute the calculateArea function and direct the user to the seed-plan page.
 document.getElementsByTagName('button')[0].addEventListener('click', function(){
-    calculateArea();
+    
+    let length = document.getElementById('length').value;
+    let width = document.getElementById('width').value;
+    document.getElementById('area').innerText = calculateArea(length, width);
+
+    let vegetable = getVegetable();
+    switch(vegetable){
+        case 'onion':
+            rowGap = vegetables.onion.rowGap;
+            bulbGap = vegetables.onion.bulbGap;
+            depth = vegetables.onion.depth;
+            plantSeasons = vegetables.onion.plantSeasons;
+            harvestSeasons = vegetables.onion.harvestSeasons;
+            break;
+        case 'garlic':
+            rowGap = vegetables.garlic.rowGap;
+            bulbGap = vegetables.garlic.bulbGap;
+            depth = vegetables.garlic.depth;
+            plantSeasons = vegetables.garlic.plantSeasons;
+            harvestSeasons = vegetables.garlic.harvestSeasons;
+            break;
+        case 'radish':
+            rowGap = vegetables.radish.rowGap;
+            bulbGap = vegetables.radish.bulbGap;
+            depth = vegetables.radish.depth;
+            plantSeasons = vegetables.radish.plantSeasons;
+            harvestSeasons = vegetables.radish.harvestSeasons;
+            break;
+        case 'parsnip':
+            rowGap = vegetables.parsnip.rowGap;  
+            bulbGap = vegetables.parsnip.bulbGap;
+            depth = vegetables.parsnip.depth;
+            plantSeasons = vegetables.parsnip.plantSeasons;
+            harvestSeasons = vegetables.parsnip.harvestSeasons;                        
+    }
+    document.getElementById('rows').innerText = calculateRows(width, rowGap);
 
 })
 
@@ -45,20 +80,33 @@ document.getElementsByTagName('button')[0].addEventListener('click', function(){
  * Calculates the user's plantable area for their chosen vegetable bulbs.
  * @param {Number} length The vegetable bed length in metres.
  * @param {Number} width The vegetable bed width in metres.
+ * @returns the area of the bed
  */
 function calculateArea(length, width) {
-    length = document.getElementById('length').value;
-    width = document.getElementById('width').value;
-    document.getElementById('area').innerText = length * width;
+    return length * width;
 }
 
+/**
+ * 
+ * @returns the user's selected vegetable from the radio button list. 
+ */
 function getVegetable() {
     let radios = document.getElementsByTagName('input');
     for (radio of radios) {
         if (radio.checked) {
-            console.log(radio.id);
+            return radio.id;
+        } else {
+            //validation check
         }
     }
 }
 
-
+/**
+ * Calculates the maximum number of row possible for the width of the vegetable bed.
+ * @param {Number} width The vegetable bed width in metres.
+ * @param {Number} rowGap The reccomended gap between rows of the specified bulb.
+ * @returns the numbner of rows
+ */
+function calculateRows(width, rowGap) {
+    return Math.floor(width / rowGap);
+}
